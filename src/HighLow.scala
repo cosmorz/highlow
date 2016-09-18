b993f6b638fbce9a925fc3ee0040b216cc4aa687
cosmorz <cosmorz@gmail.com>
2016-09-18 17:36:08 +0900

    コメント追加

--- src/HighLow.scala
+++ src/HighLow.scala
@@ -1,22 +1,28 @@
 import scala.util.Random
 
 /**
-  * Created by kota on 2016/09/17.
+  * HIGH&LOW ゲーム
   */
 object HighLow {
   def main(args: Array[String]): Unit = {
 
     val cards: List[Card] = Random.shuffle((1 to 13).map(n => Card(n))).toList
 
-    val result = game(cards.head, cards.tail, 5)
-
-    if (result) {
+    if (game(cards.head, cards.tail, 5)) {
       println("Clear!")
     } else {
       println("Wrong...")
     }
   }
 
+  /**
+    * HIGH&LOW 本体
+    *
+    * @param card  場札
+    * @param deck  山札
+    * @param count 残りゲーム回数
+    * @return ゲーム結果
+    */
   def game(card: Card, deck: List[Card], count: Int): Boolean = {
     println("Game Count: " + count)
 
@@ -42,17 +48,21 @@ object HighLow {
     }
   }
 
+  /**
+    * カード比較処理
+    *
+    * @param c1 カード
+    * @param c2 カード
+    * @return 比較結果
+    */
   def higher(c1: Card, c2: Card): Boolean = {
     c1.number > c2.number
   }
 }
 
-case class Card(number: Int) {
-  def <(card: Card): Boolean = {
-    this.number < card.number
-  }
-
-  def >(card: Card): Boolean = {
-    this.number > card.number
-  }
-}
+/**
+  * カード クラス
+  *
+  * @param number
+  */
+case class Card(number: Int)
